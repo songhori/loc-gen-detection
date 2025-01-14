4c2797d610b4ab7b0cd17a47adadae8ed4239483
songhori <m.songhori@gmail.com>
2025-01-14 12:25:55 +0330

    removed image paths that does not exist in data folders

--- update_data.py
+++ update_data.py
@@ -13,6 +13,7 @@ existing_paths = set(train_df['path'].values)
 
 # Collect new image paths
 new_entries = []
+existing_image_paths = set()
 
 # Iterate over each class folder and its images
 for class_folder in sorted(os.listdir(image_folder_path)):
@@ -20,6 +21,7 @@ for class_folder in sorted(os.listdir(image_folder_path)):
     if os.path.isdir(class_path):
         for image_file in sorted(os.listdir(class_path)):
             image_path = f"{class_folder}/{image_file}"
+            existing_image_paths.add(image_path)
             if image_path not in existing_paths:
                 new_entries.append([int(class_folder), image_path, -1, -1])
 
@@ -29,8 +31,14 @@ new_entries_df = pd.DataFrame(new_entries, columns=["class", "path", "male", "fe
 # Append new entries to the existing DataFrame
 updated_train_df = pd.concat([train_df, new_entries_df], ignore_index=True)
 
-# Save the updated CSV
+# Remove rows with paths that don't exist in the images directory
+updated_train_df = updated_train_df[updated_train_df['path'].isin(existing_image_paths)]
+
+# Sort the DataFrame by the 'class' column
+updated_train_df = updated_train_df.sort_values(by='class')
+
+# Save the updated and sorted CSV
 updated_csv_path = "data/train2.csv"
 updated_train_df.to_csv(updated_csv_path, index=False)
 
-print(f"Updated train.csv saved as {updated_csv_path}")
+print(f"Updated train2.csv saved as {updated_csv_path}")
